2133da14cbd63359c4fcb6186143427029ec53e0
JeonMinSu <31675789+JeonMinSu@users.noreply.github.com>
2023-06-04 00:34:41 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: [
     {
       name: `Minsu`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/JeonMinSu",
     },
   ],
   // blog setting (required)
