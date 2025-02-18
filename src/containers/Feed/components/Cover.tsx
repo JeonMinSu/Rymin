b28a32661ff34819442f9af7eb847c662696bc80
JeonMinSu <session_min@naver.com>
2025-02-19 03:01:55 +0900

    [전민수] 배너 테스트

--- src/containers/Feed/components/Cover.tsx
+++ src/containers/Feed/components/Cover.tsx
@@ -1,13 +1,29 @@
 // 클라이언트에서 API 호출
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router"
+import { getPageCover } from "@/src/libs/apis/getCover";
 
 const Cover: React.FC = () => {
   const router = useRouter();
+  const [coverUrl, setCoverUrl] = useState();
 
   console.log('Cover.tsx: Cover: React.FC'+ router.query.coverUrl);
-  // const [coverUrl, setCoverUrl] = useState<string | null>(null);
-  // const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    async function fetchCover() {
+      // try {
+      //   const cover = await getPageCover();
+
+      //   if (cover) {
+      //     setCoverUrl(coverUrl);
+      //   } else {
+      //     console.error("Failed to load cover.");
+      //   }
+      // } catch (error) {
+      //   console.error("Failed to load cover.");
+      // }
+    }
+    fetchCover();
+  })
 
   // useEffect(() => {
   //   async function fetchCover() {
