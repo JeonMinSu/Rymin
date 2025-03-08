6d92545df938381716af2f5e6acce0ea0a887c88
JeonMinSu <session_min@naver.com>
2025-03-09 00:13:44 +0900

    [전민수] 레이아웃 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,55 +9,80 @@ import { NextPageWithLayout } from "./_app"
 import { TCategories, TPosts, TTags } from "../types"
 import { getPosts } from "../libs/apis"
 import { DEFAULT_CATEGORY } from "../constants"
+import { NotionRenderer } from "react-notion-x"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 
-export async function getStaticProps() {
-  try {
-    const posts = await getPosts()
-    const filteredPost = filterPosts(posts)
-    const tags = getAllSelectItemsFromPosts("tags", filteredPost)
-    const categories = getAllSelectItemsFromPosts("category", filteredPost)
+// export async function getStaticProps() {
+//   try {
+//     const posts = await getPosts()
+//     const filteredPost = filterPosts(posts)
+//     const tags = getAllSelectItemsFromPosts("tags", filteredPost)
+//     const categories = getAllSelectItemsFromPosts("category", filteredPost)
 
-    return {
-      props: {
-        tags: {
-          ...tags,
-        },
-        categories: {
-          [DEFAULT_CATEGORY]: filteredPost.length,
-          ...categories,
-        },
-        posts: filteredPost,
-      },
-      revalidate: 1,
-    }
-  } catch (error) {
-    throw error
-  }
-}
+//     return {
+//       props: {
+//         tags: {
+//           ...tags,
+//         },
+//         categories: {
+//           [DEFAULT_CATEGORY]: filteredPost.length,
+//           ...categories,
+//         },
+//         posts: filteredPost,
+//       },
+//       revalidate: 1,
+//     }
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-type Props = {
-  categories: TCategories
-  tags: TTags
-  posts: TPosts
-}
+// type Props = {
+//   categories: TCategories
+//   tags: TTags
+//   posts: TPosts
+// }
 
-const FeedPage: NextPageWithLayout<Props> = ({ categories, tags, posts }) => {
-  return <Feed categories={categories} tags={tags} posts={posts} />
-}
+// const FeedPage: NextPageWithLayout<Props> = ({ categories, tags, posts }) => {
+//   return <Feed categories={categories} tags={tags} posts={posts} />
+// }
 
-FeedPage.getLayout = function getlayout(page) {
+// FeedPage.getLayout = function getlayout(page) {
+//   return (
+//     <Layout
+//       metaConfig={{
+//         title: CONFIG.blog.title,
+//         description: CONFIG.blog.description,
+//         type: "website",
+//         url: CONFIG.link,
+//       }}
+//     >
+//       {page}
+//     </Layout>
+//   )
+// }
+export default function FeedPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [recordMap, setRecordMap] = useState<any>(null);
+
+  useEffect(() => {
+      if (id) {
+          fetch(`/api/getPage?pageId=${id}`)
+              .then((res) => res.json())
+              .then((data) => setRecordMap(data))
+              .catch((err) => console.error("Error fetching Notion data:", err));
+      }
+  }, [id]);
+
+  if (!recordMap) return <p>Loading...</p>;
+
   return (
-    <Layout
-      metaConfig={{
-        title: CONFIG.blog.title,
-        description: CONFIG.blog.description,
-        type: "website",
-        url: CONFIG.link,
-      }}
-    >
-      {page}
-    </Layout>
-  )
+      <div className="p-6">
+          <NotionRenderer recordMap={recordMap} fullPage darkMode={false} />
+      </div>
+  );
 }
 
-export default FeedPage
+// export default FeedPage
