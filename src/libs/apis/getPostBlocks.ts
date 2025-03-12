f795b47ed7caaf05bcae27416ce0fa0cd975d121
JeonMinSu <session_min@naver.com>
2025-03-12 14:18:54 +0900

    [전민수] FeedPage 롤백

--- src/libs/apis/getPostBlocks.ts
+++ src/libs/apis/getPostBlocks.ts
@@ -1,18 +1,7 @@
 import { NotionAPI } from "notion-client"
 
-const api = new NotionAPI()
-export async function getPostBlocks(pageId: string) {
-
-  try{
-    if (!pageId || typeof pageId !== "string") {
-      throw new Error("Invalid pageId")
-    }
-
-    const pageBlock = await api.getPage(pageId)
-    return pageBlock
-
-  } catch (error) {
-    console.error("Error fetching Notion data:", error)
-    return null;
-  }
+export async function getPostBlocks(id: string) {
+  const api = new NotionAPI()
+  const pageBlock = await api.getPage(id)
+  return pageBlock
 }
