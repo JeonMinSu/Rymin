d61283ececfd01a303c4c60672a237aef9a3700b
JeonMinSu <session_min@naver.com>
2025-04-29 12:30:45 +0900

    [전민수] 필터 로직 복구

--- src/libs/utils/notion/filterBlockMap.ts
+++ src/libs/utils/notion/filterBlockMap.ts
@@ -9,44 +9,44 @@ const initialOption:Options = {
     aceeptStatus: ["Public"]
 }
 
-export function filterPublicBlocks(blockMap: ExtendedRecordMap, options: Options = initialOption): ExtendedRecordMap {
+// export function filterPublicBlocks(blockMap: ExtendedRecordMap, options: Options = initialOption): ExtendedRecordMap {
 
-    const { aceeptStatus = ["Public"] } = options
+//     const { aceeptStatus = ["Public"] } = options
 
-    const filteredBlockMap = Object.fromEntries(
-        Object.entries(blockMap.block).filter(([id, block]) => {
-            const properties = block?.value?.properties
-            const statusArray = properties?.['status']
+//     const filteredBlockMap = Object.fromEntries(
+//         Object.entries(blockMap.block).filter(([id, block]) => {
+//             const properties = block?.value?.properties
+//             const statusArray = properties?.['status']
 
-            const status = Array.isArray(statusArray) && statusArray[0] && statusArray[0][0]
+//             const status = Array.isArray(statusArray) && statusArray[0] && statusArray[0][0]
 
-            const ispublic = !status || aceeptStatus.includes(status)
+//             const ispublic = !status || aceeptStatus.includes(status)
 
-            return ispublic
-        })
-    )
-    return {
-        ...blockMap,
-        block: filteredBlockMap,
-    }
-}
-
-// export function filterPublicBlocks(blockMap: ExtendedRecordMap): ExtendedRecordMap {
-
-//   const filteredBlockMap = Object.fromEntries(
-//     Object.entries(blockMap.block).filter(([id, block]) => {
-//       const properties = block?.value?.properties
-//       const statusArray = properties?.['status']
-
-//       const status = Array.isArray(statusArray) && statusArray[0] && statusArray[0][0]
-
-//       const ispublic = !status || status === 'Public'
-
-//       return ispublic
-//     })
-//   )
+//             return ispublic
+//         })
+//     )
 //     return {
 //         ...blockMap,
 //         block: filteredBlockMap,
 //     }
 // }
+
+export function filterPublicBlocks(blockMap: ExtendedRecordMap): ExtendedRecordMap {
+
+  const filteredBlockMap = Object.fromEntries(
+    Object.entries(blockMap.block).filter(([id, block]) => {
+      const properties = block?.value?.properties
+      const statusArray = properties?.['status']
+
+      const status = Array.isArray(statusArray) && statusArray[0] && statusArray[0][0]
+
+      const ispublic = !status || status === 'Public'
+
+      return ispublic
+    })
+  )
+    return {
+        ...blockMap,
+        block: filteredBlockMap,
+    }
+}
